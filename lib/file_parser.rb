ab092c59527d3a34be9ef41ebc28e2b38bdecdbd
nrakochy <nick.rakochy@gmail.com>
2015-02-03 17:37:47 -0600

    Added capacity to combine all parsed files into single formatted file in FileParser

--- lib/file_parser.rb
+++ lib/file_parser.rb
@@ -1,15 +1,19 @@
 class FileParser
-  attr_reader :files_to_parse
+  attr_reader :files_to_parse, :formatted_file
 
   def initialize(files_to_parse)
     @files_to_parse = files_to_parse
+    @formatted_file = File.open("formatted_file.txt", "w")
   end
 
-  def read_file(data_file)
+  def parse_and_combine_files
+    @files_to_parse.each { |file| reformat_to_single_file(file) }
+    @formatted_file.close
+  end
+
+  def reformat_to_single_file(data_file)
     File.open(data_file, "r") do |file|
-      file.readlines.map do |line|
-        reformat_line(line)
-      end
+      file.readlines.map{ |line| @formatted_file << reformat_line(line) }
     end
   end
 
