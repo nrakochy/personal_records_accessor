35394ddd1fed470b9b4d5c38e6f278fc16cf5167
nrakochy <nick.rakochy@gmail.com>
2015-02-04 15:44:54 -0600

    Moved FileFormatter to FileParser. Added convert_line_to_hash as the returned value of FileParser class

---
+++ lib/file_formatter.rb
@@ -0,0 +1,52 @@
+<<<<<<< HEAD:lib/file_parser.rb
+class FileParser
+  attr_reader :files_to_parse, :formatted_records
+=======
+class FileFormatter
+  attr_reader :files_to_parse, :formatted_file
+>>>>>>> 13090c580d928426284ff01690830445702deb0e:lib/file_formatter.rb
+
+  def initialize(files_to_parse)
+    @files_to_parse = files_to_parse
+    @formatted_records = []
+  end
+
+  def parse_files
+    @files_to_parse.each { |file| reformat_data_file(file) }
+    @formatted_records
+  end
+
+  def reformat_data_file(data_file)
+    File.open(data_file, "r") do |file|
+      file.readlines.map do |line|
+        formatted_line = reformat_line(line)
+        @formatted_records << convert_line_to_hash(formatted_line)
+      end
+    end
+  end
+
+  def convert_line_to_hash(params)
+     {
+     last_name: params[0],
+     first_name: params[1],
+     gender: params[2],
+     favorite_color: params[3],
+     date_of_birth: params[4]
+    }
+  end
+
+  def reformat_line(line)
+    chomped_line = line.chomp
+    trimmed_line = trim_line(chomped_line)
+    remove_superfluous_spaces(trimmed_line)
+  end
+
+  def trim_record_line(line)
+    line.tr(',|', " ")
+  end
+
+  def split_line_by_category(line)
+    line.split(" ")
+  end
+
+end
--- spec/file_formatter_spec.rb
+++ spec/file_formatter_spec.rb
@@ -1,10 +1,12 @@
-require 'file_parser'
+require 'file_formatter'
 
-describe FileParser do
-  let(:parser){ FileParser.new(["example_file"]) }
+describe FileFormatter do
+  let(:parser){ FileFormatter.new(["example_file"]) }
   let(:sample_line_pipes){ "LastName | FirstName | Gender | FavoriteColor | DateOfBirth" }
   let(:sample_line_commas){ "LastName, FirstName, Gender, FavoriteColor, DateOfBirth" }
   let(:sample_line_spaces){ "LastName FirstName Gender FavoriteColor DateOfBirth" }
+  let(:formatted_line){ ["LastName", "FirstName", "Gender", "FavoriteColor", "DateOfBirth"] }
+  let(:formatted_hash){ {last_name: "LastName", first_name: "FirstName", gender: "Gender", favorite_color: "FavoriteColor", date_of_birth: "DateOfBirth"} }
 
   describe "#trim_record_line" do
     it 'removes record delimiters and replaces them with a space' do
@@ -13,13 +15,20 @@ describe FileParser do
     end
   end
 
-  describe "#remove_superfluous_spaces_from_line" do
+  describe "#split_line_by_category" do
     it 'removes record delimiters and replaces them with a space' do
       trimmed_pipe_line = parser.trim_record_line(sample_line_pipes)
       trimmed_comma_line = parser.trim_record_line(sample_line_commas)
-      expect(parser.remove_superfluous_spaces_from_line(trimmed_pipe_line)).to eq(sample_line_spaces)
-      expect(parser.remove_superfluous_spaces_from_line(trimmed_comma_line)).to eq(sample_line_spaces)
+      expect(parser.split_line_by_category(trimmed_pipe_line)).to eq(formatted_line)
+      expect(parser.split_line_by_category(trimmed_comma_line)).to eq(formatted_line)
     end
   end
+
+  describe "#convert_line_to_hash" do
+    it 'creates a Hash with data record assigned as values' do
+      expect(parser.convert_line_to_hash(formatted_line)).to eq(formatted_hash)
+    end
+  end
+
 
 end
