ab567f8f2b9d0d6b47e82c5755f123390f98a334
nrakochy <nick.rakochy@gmail.com>
2015-02-27 13:41:33 -0600

    Updated db. Created test.json for repo_spec testing.

--- spec/json_repository_spec.rb
+++ spec/json_repository_spec.rb
@@ -3,26 +3,26 @@ require_relative '../lib/json_repository'
 
 describe JSONRepository::User do
 
-  let(:record1){ {last_name: "LastName", first_name: "FirstName", gender: "female", favorite_color: "black", date_of_birth: "03/01/1980"}.to_json }
   let(:record1_hash){ {last_name: "LastName", first_name: "FirstName", gender: "female", favorite_color: "black", date_of_birth: "03/01/1980"} }
+  let(:record1){ {last_name: "LastName", first_name: "FirstName", gender: "female", favorite_color: "black", date_of_birth: "03/01/1980"}.to_json }
   let(:record2){ {last_name: "LastName2", first_name: "FirstName2", gender: "female", favorite_color: "red", date_of_birth: "03/02/1980"}.to_json }
   let(:record3){ {last_name: "LastName3", first_name: "FirstName3", gender: "female", favorite_color: "orange", date_of_birth: "03/03/1980"}.to_json }
   let(:record4){ {last_name: "LastName4", first_name: "FirstName4", gender: "female", favorite_color: "blue", date_of_birth: "03/04/1980"}.to_json }
-  let(:db_path){ "test.json" }
-  let(:repo){ YAMLRepository::User.new({ db_path: db_path }) }
+  let(:db_path){ "./spec/support/test.json" }
+  let(:repo){ JSONRepository::User.new({ db_path: db_path }) }
 
   before :each do
-    @test_file = create_yaml_file
+    @test_file = create_test_file
   end
 
   describe "#convert_record_to_yaml" do
-    it 'correctly converts a Hash to Yaml' do
+    it 'correctly converts a Hash to JSON' do
       expect(repo.convert_record_format(record1_hash)).to eq(record1)
     end
   end
 
   describe "#find_all_records" do
-    it 'correctly retrieves yml file contents' do
+    it 'correctly retrieves file contents' do
       expect(repo.find_all_records).to eq([record1, record2])
     end
   end
@@ -35,7 +35,7 @@ describe JSONRepository::User do
 
 end
 
-def create_yaml_file
+def create_test_file
   test_data = [record1, record2]
-  File.open(db_path, 'w') { |file| YAML.dump(test_data, file) }
+  File.open(db_path, 'w') { |file| file.write(test_data) }
 end
