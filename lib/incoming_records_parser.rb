77e9ffd1f6d0d578b68f0d378d9e46f38da76bf4
nrakochy <nick.rakochy@gmail.com>
2015-03-04 10:30:06 -0600

    Added JSON post parsing to IncomingRecordsParser

--- lib/incoming_records_parser.rb
+++ lib/incoming_records_parser.rb
@@ -1,27 +1,11 @@
+require 'json'
+
 class IncomingRecordsParser
   attr_reader :formatted_records
 
   def initialize
     @formatted_records = []
   end
-
-  def parse_files(unparsed_files)
-    unparsed_files.each { |file| reformat_data_file(file) }
-    @formatted_records
-  end
-
-  def reformat_data_file(data_file)
-    File.open(data_file, "r") do |file|
-      file.readlines.map{
-        |line| @formatted_records << reformat_line_and_convert_to_record(line) }
-    end
-  end
-
-  def reformat_line_and_convert_to_record(line)
-    reformatted_line = reformat_line(line)
-    convert_line_to_hash(reformatted_line)
-  end
-
 
   def convert_line_to_hash(params)
      {
@@ -33,18 +17,43 @@ class IncomingRecordsParser
     }
   end
 
+  def parse_files(unparsed_files)
+    unparsed_files.each { |file| reformat_data_file(file) }
+    @formatted_records
+  end
+
+  def parse_post_request(data_record)
+    parsed_record = parse_JSON_record(data_record)
+    posted_data = parsed_record[:record]
+    reformat_line_and_convert_to_hash(posted_data)
+  end
+
+  def parse_JSON_record(record)
+    JSON.parse(record, :symbolize_names => true)
+  end
+
+  def reformat_data_file(data_file)
+    File.open(data_file, "r") do |file|
+      file.readlines.map{ |line| @formatted_records << reformat_line_and_convert_to_hash(line) }
+    end
+  end
+
+  def reformat_line_and_convert_to_hash(line)
+    reformatted_line = reformat_line(line)
+    convert_line_to_hash(reformatted_line)
+  end
+
   def reformat_line(line)
     chomped_line = line.chomp
     trimmed_line = trim_record_line(chomped_line)
     split_line_by_category(trimmed_line)
   end
 
-  def trim_record_line(line)
-    line.tr(',|', " ")
-  end
-
   def split_line_by_category(line)
     line.split(" ")
   end
 
+  def trim_record_line(line)
+    line.tr(',|', " ")
+  end
 end
--- spec/incoming_records_parser_spec.rb
+++ spec/incoming_records_parser_spec.rb
@@ -6,22 +6,25 @@ describe IncomingRecordsParser do
   let(:sample_line_pipes){ "LastName | FirstName | Gender | FavoriteColor | DateOfBirth" }
   let(:sample_line_commas){ "LastName, FirstName, Gender, FavoriteColor, DateOfBirth" }
   let(:sample_line_spaces){ "LastName FirstName Gender FavoriteColor DateOfBirth" }
+  let(:json_post_request){ { :record => sample_line_commas }.to_json }
   let(:formatted_line){ ["LastName", "FirstName", "Gender", "FavoriteColor", "DateOfBirth"] }
   let(:formatted_hash){ {last_name: "LastName", first_name: "FirstName", gender: "Gender", favorite_color: "FavoriteColor", date_of_birth: "DateOfBirth"} }
 
-  describe "#trim_record_line" do
-    it 'removes record delimiters and replaces them with a space' do
-      expect(parser.trim_record_line(sample_line_pipes)).to eq("LastName   FirstName   Gender   FavoriteColor   DateOfBirth")
-      expect(parser.trim_record_line(sample_line_commas)).to eq("LastName  FirstName  Gender  FavoriteColor  DateOfBirth")
+  describe "#convert_line_to_hash" do
+    it 'creates a Hash with data record assigned as values' do
+      expect(parser.convert_line_to_hash(formatted_line)).to eq(formatted_hash)
     end
   end
 
-  describe "#split_line_by_category" do
-    it 'removes record delimiters and replaces them with a space' do
-      trimmed_pipe_line = parser.trim_record_line(sample_line_pipes)
-      trimmed_comma_line = parser.trim_record_line(sample_line_commas)
-      expect(parser.split_line_by_category(trimmed_pipe_line)).to eq(formatted_line)
-      expect(parser.split_line_by_category(trimmed_comma_line)).to eq(formatted_line)
+  describe "#parse_JSON_record" do
+    it 'converts a JSON record to a Ruby hash with symbolized key' do
+      expect(parser.parse_JSON_record(json_post_request)).to eq({record: sample_line_commas})
+    end
+  end
+
+  describe "#parse_post_request" do
+    it 'converts an incoming post request into the db format' do
+      expect(parser.parse_post_request(json_post_request)).to eq(formatted_hash)
     end
   end
 
@@ -33,16 +36,19 @@ describe IncomingRecordsParser do
     end
   end
 
-
-  describe "#convert_line_to_hash" do
-    it 'creates a Hash with data record assigned as values' do
-      expect(parser.convert_line_to_hash(formatted_line)).to eq(formatted_hash)
+describe "#split_line_by_category" do
+    it 'removes record delimiters and replaces them with a space' do
+      trimmed_pipe_line = parser.trim_record_line(sample_line_pipes)
+      trimmed_comma_line = parser.trim_record_line(sample_line_commas)
+      expect(parser.split_line_by_category(trimmed_pipe_line)).to eq(formatted_line)
+      expect(parser.split_line_by_category(trimmed_comma_line)).to eq(formatted_line)
     end
   end
 
-  describe "#convert_line_to_hash" do
-    it 'creates a Hash with data record assigned as values' do
-      expect(parser.convert_line_to_hash(formatted_line)).to eq(formatted_hash)
+  describe "#trim_record_line" do
+    it 'removes record delimiters and replaces them with a space' do
+      expect(parser.trim_record_line(sample_line_pipes)).to eq("LastName   FirstName   Gender   FavoriteColor   DateOfBirth")
+      expect(parser.trim_record_line(sample_line_commas)).to eq("LastName  FirstName  Gender  FavoriteColor  DateOfBirth")
     end
   end
 
