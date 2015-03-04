669440deebbae3c095918181d08ec02430b14379
nrakochy <nick.rakochy@gmail.com>
2015-03-04 12:45:02 -0600

    Refactored and reorganized APIMediator for clearer api methods

--- lib/api_mediator.rb
+++ lib/api_mediator.rb
@@ -2,10 +2,10 @@ require 'console_presenter'
 require 'data_record_sorter'
 require 'endpoint_presenter'
 require 'incoming_records_parser'
-require 'json_repository'
 require 'personal_record'
 require 'query_requirements'
 require 'record_constructor'
+require 'repository_interface'
 
 class APIMediator
 
@@ -15,61 +15,93 @@ class APIMediator
     @record_sorter = DataRecordSorter.new
     @record_constructor = RecordConstructor.new
     @repo = RepositoryInterface.for(:records)
-    @query_requirements = QueryRequirements.new({ record_sorter: @record_sorter })
+    @console_query_requirements = QueryRequirements.new({ record_sorter: @record_sorter })
     @console_presenter = ConsolePresenter.new
     @endpoint_presenter = EndpointPresenter.new
-  end
-
-  def construct_personal_record(data_record)
-    @record_constructor.create_personal_record(data_record)
   end
 
   def build_personal_records(data_records)
     data_records.map{ |record| construct_personal_record(record) }
   end
 
+  def get_all_db_records
+    all_records = find_all_records
+    present_results_to_endpoint(all_records)
+  end
+
+  def get_records_sorted_by_query(sort_option)
+    all_data = find_all_records
+    records = build_personal_records(all_data)
+    results = sort_records_by_param(sort_option, records)
+    formatted_results = read_personal_records_data(results)
+    present_results_to_endpoint(formatted_results)
+  end
+
+  def process_records_and_display_to_console
+    records_information = parse_files(@data_files)
+    data_records = build_personal_records(records_information)
+    save_new_records(data_records)
+    sorted_records = sort_records_acc_to_query_requirements(data_records)
+    display_to_console(sorted_records)
+  end
+
+  def save_new_records(data_records)
+    query_field = :last_name
+    sorted_records = sort_records_by_param(query_field, data_records)
+    save_records(sorted_records)
+  end
+
+  def save_posted_record(data)
+    parsed_data = parse_post_request(data)
+    record = construct_personal_record(parsed_data)
+    saved_record = save_record(record)
+    present_results_to_endpoint([saved_record])
+  end
+
+  private
+
+  def construct_personal_record(data_record)
+    @record_constructor.create_personal_record(data_record)
+  end
+
   def display_to_console(sorted_results)
     @console_presenter.display_all_sets_of_records(sorted_results)
   end
 
-  def get_all_db_records
-    all_records = @repo.find_all_records
-    @endpoint_presenter.format_http_response(all_records)
+  def find_all_records
+    @repo.find_all_records
   end
 
-  def get_records_sorted_by_query(sort_option)
-    all_data = @repo.find_all_records
-    records = build_personal_records(all_data)
-    results = @record_sorter.sort_records_ascending(sort_option, records)
-    formatted_results = read_personal_records_data(results)
+  def present_results_to_endpoint
     @endpoint_presenter.format_http_response(formatted_results)
+  end
+
+  def parse_post_request(data)
+    @parser.parse_post_request
   end
 
   def parse_files(data_files)
     @parser.parse_files(data_files)
   end
 
-  def process_records_and_display_to_console
-    records_information = parse_files(@data_files)
-    data_records = build_personal_records(records_information)
-    save_records(data_records)
-    sorted_records = sort_records(data_records)
-    display_to_console(sorted_records)
-  end
-
   def read_personal_records_data(data_records)
     data_records.map{|record| record.read_record_attributes_without_reformatted_date_included }
   end
 
+  def save_record(data_record)
+    @repo.save(data_record)
+  end
+
   def save_records(data_records)
-    query_field = :last_name
-    sorted_records = @record_sorter.sort_records_ascending(query_field, data_records)
-    @repo.overwrite_data_records_file(sorted_records)
+    @repo.overwrite_data_records_file(data_records)
   end
 
-  def sort_records(data_records)
-    @query_requirements.sort_by_query_requirements(data_records)
+  def sort_records_acc_to_query_requirements(data_records)
+    @console_query_requirements.sort_by_query_requirements(data_records)
   end
 
+  def sort_records_by_param(param, data_records)
+    @record_sorter.sort_records_ascending(param, data_records)
+  end
 end
 
